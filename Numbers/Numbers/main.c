b8e9eeeb8cb1fbef0b4b14b41dc89c512ebef0ac
jimtoepel <jim.toepel@gmail.com>
2015-09-19 18:50:02 -0700

    Destroyed the Challenge

--- Numbers/Numbers/main.c
+++ Numbers/Numbers/main.c
@@ -8,12 +8,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, const char * argv[]) {
 
-    printf("11 / 3 = %d remainder of %d\n", 11 / 3, 11 % 3);
-    printf("11 / 3.0 = %f\n", 11 / (float)3);
-    printf("The absolute value of -5 is %d\n", abs(-5));
+    double y = 12345.6789;
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
+    
+    double rad = 1;
+    double sinOfRad = sin(rad);
+    printf("Sine of %.2f rad is %.3f\n", rad, sinOfRad);
     
     
     return 0;
