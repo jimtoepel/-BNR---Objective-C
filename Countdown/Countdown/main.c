ca4a8828144751ccfed1dbcdda8690e346b383dc
jimtoepel <jim.toepel@gmail.com>
2015-09-21 21:55:02 -0700

    Quick prep for tomorrow

--- Countdown/Countdown/main.c
+++ Countdown/Countdown/main.c
@@ -10,6 +10,9 @@
 
 int main(int argc, const char * argv[]) {
 
+    printf("Hi, what number should I count down from?\n\n");
+    
+    
     int start = 99;
     
     for (int i = start; i >= 0; i = i- 3) {
