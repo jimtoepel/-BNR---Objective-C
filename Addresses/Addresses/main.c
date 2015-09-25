db96d44830a8f7369522bf2c27c64d3af2ca25e1
jimtoepel <jim.toepel@gmail.com>
2015-09-24 20:34:04 -0700

    memory addresses
    short sizes are tomorrow

--- Addresses/Addresses/main.c
+++ Addresses/Addresses/main.c
@@ -14,12 +14,19 @@ int main(int argc, const char * argv[]) {
     int i = 17;
     int *addressOfI = &i;
     
+    float decimal = 3.1459;
+    
     printf("i stores its value at %p\n", addressOfI);
     printf("this function starts at %p\n", main);
     printf("the int stored at addressOfI is %d\n", *addressOfI);
     
     *addressOfI = 89;
     printf("Now i is %d\n", i);
+    printf("An int is %zu bytes\n", sizeof(int));
+    printf("A pointer is %zu bytes\n", sizeof(int *));
+    
+    printf("A float is %zu bytes\n", sizeof(decimal));
+    printf("The pointer is %zu bytes\n", sizeof(float *));
     
     return 0;
 }
