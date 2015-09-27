44a9b356bf4be89d7b94401c378ea440a7ae12d2
jimtoepel <jim.toepel@gmail.com>
2015-09-27 14:16:10 -0700

    Challenge met!

--- PBR/PBR/main.c
+++ PBR/PBR/main.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <math.h>
 
-void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr)
+void metersToFeetAndInches(double meters, double *ftPtr, double *inPtr)
 {
     // This assumes non-negative meters
     
@@ -17,29 +17,26 @@ void metersToFeetAndInches(double meters, unsigned int *ftPtr, double *inPtr)
     double rawFeet = meters * 3.281;
     
     // How many complete feet as an unsigned int?
-    unsigned int feet = (unsigned int)floor(rawFeet);
-    
-    // Store the number of feet at the supplied address
-    printf("Storing %u to the address %p\n", feet, ftPtr);
-    *ftPtr = feet;
+    double rawinches = modf(rawFeet, ftPtr);
+    double inches = rawinches * 12;
     
     // Calculate inches
-    double fractionalFoot = rawFeet - feet;
-    double inches = fractionalFoot * 12.0;
     
     // Store that number of inches in the referenced location
-    printf("Storing %.2f to the address %p\n", inches, inPtr);
-    *inPtr = inches;
+    if (inPtr) {
+        printf("Storing %.2f to the address %p\n", inches, inPtr);
+        *inPtr = inches;
+    }
 }
 
 int main(int argc, const char * argv[]) {
 
     double          meters = 3.0;
-    unsigned int    feet;
+    double          feet;
     double          inches;
     
     metersToFeetAndInches(meters, &feet, &inches);
-    printf("%.1f meters is equal to %d feet and %.1f inches.", meters, feet, inches);
+    printf("%.1f meters is equal to %f feet and %.1f inches.", meters, feet, inches);
     
     return 0;
 }
