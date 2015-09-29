f26607a8fcf3652b3369973c9b5db4ecca4f1feb
jimtoepel <jim.toepel@gmail.com>
2015-09-28 17:28:48 -0700

    Structs!
    Build objects!

---
+++ BMICalc/BMICalc/main.c
@@ -0,0 +1,35 @@
+//
+//  main.c
+//  BMICalc
+//
+//  Created by Jim Toepel on 9/28/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#include <stdio.h>
+
+// Here is the declaration of the struct
+
+struct Person {
+    float heightInMeters;
+    int weightInKilos;
+};
+
+int main(int argc, const char * argv[]) {
+
+    struct Person mikey;
+    mikey.heightInMeters = 1.7;
+    mikey.weightInKilos = 96;
+    
+    struct Person aaron;
+    aaron.heightInMeters = 1.97;
+    aaron.weightInKilos = 84;
+    
+    printf("mikey is %.2f meters tall\n", mikey.heightInMeters);
+    printf("mikey weighs %d kilograms\n", mikey.weightInKilos);
+    
+    printf("aaron is %.2f meters tall\n", aaron.heightInMeters);
+    printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
+    
+    return 0;
+}
