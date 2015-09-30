082b5656f784dc1582dca095115e4d00554b2545
jimtoepel <jim.toepel@gmail.com>
2015-09-29 21:18:35 -0700

    Structs

--- BMICalc/BMICalc/main.c
+++ BMICalc/BMICalc/main.c
@@ -10,26 +10,34 @@
 
 // Here is the declaration of the struct
 
-struct Person {
+
+// Declare the type Person
+typedef struct {
     float heightInMeters;
     int weightInKilos;
-};
+} Person;
+
+float bodyMassIndex (Person p)
+{
+    return p.weightInKilos / (p.heightInMeters *p.heightInMeters);
+}
 
 int main(int argc, const char * argv[]) {
 
-    struct Person mikey;
+    Person mikey;
     mikey.heightInMeters = 1.7;
     mikey.weightInKilos = 96;
     
-    struct Person aaron;
+    Person aaron;
     aaron.heightInMeters = 1.97;
     aaron.weightInKilos = 84;
     
-    printf("mikey is %.2f meters tall\n", mikey.heightInMeters);
-    printf("mikey weighs %d kilograms\n", mikey.weightInKilos);
+    float bmi;
+    bmi = bodyMassIndex(mikey);
+    printf("mikey has a BMI of %.2f\n", bmi);
     
-    printf("aaron is %.2f meters tall\n", aaron.heightInMeters);
-    printf("aaron weighs %d kilograms\n", aaron.weightInKilos);
+    bmi = bodyMassIndex(aaron);
+    printf("aaron has a BMI of %.2f\n", bmi);
     
     return 0;
 }
