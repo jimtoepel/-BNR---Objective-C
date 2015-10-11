7e3d620478dfcbcb71418251aa3fb6ac60c9feae
jimtoepel <jim.toepel@gmail.com>
2015-10-10 19:36:38 -0700

    Started next challenge

---
+++ HowLong?/HowLong?/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  HowLong?
+//
+//  Created by Jim Toepel on 10/10/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+        NSLog(@"I was born on: \n");
+        NSLog(@"And have been alive for: \n");
+    }
+    return 0;
+}
