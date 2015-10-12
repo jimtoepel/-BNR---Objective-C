c948c986fb6ab72dc9e8c036899f6ad6c3bb5ed9
jimtoepel <jim.toepel@gmail.com>
2015-10-11 21:42:45 -0700

    Working on dates

--- HowLong?/HowLong?/main.m
+++ HowLong?/HowLong?/main.m
@@ -11,8 +11,16 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        
-        NSLog(@"I was born on: \n");
+        NSDate *now = [[NSDate alloc] init];
+        NSLog(@"The date today is: %@\n", now);
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear:1982];
+        [comps setMonth:2];
+        [comps setDay:18];
+        [comps setHour:13];
+        [comps setMinute:34];
+        [comps setSecond:0];
+        NSLog(@"I was born at: %@\n", comps);
         NSLog(@"And have been alive for: \n");
     }
     return 0;
