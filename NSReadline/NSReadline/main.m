6d563c0c7334b16a5b8c8d3e7a366ecfce780f01
jimtoepel <jim.toepel@gmail.com>
2015-10-16 20:08:59 -0700

    Started to use NS stuff

---
+++ NSReadline/NSReadline/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  NSReadline
+//
+//  Created by Jim Toepel on 10/16/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <readline/readline.h>
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        // readline the text entered (Chapter 8)
+        // readline()
+        // atoi()
+        
+        // confirm the text entered
+        
+        
+        // convert that string to a number
+        
+        
+        // LOOP
+        // Count down from that number by 3s
+        
+        
+        // Display number
+        
+        
+    }
+    return 0;
+}
