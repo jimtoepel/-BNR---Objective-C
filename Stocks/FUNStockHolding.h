6bb7724899c258d0bcafa24c1293d857f7012c79
jimtoepel <jim.toepel@gmail.com>
2015-10-20 19:42:18 -0700

    Setup the project for challenge 18

---
+++ Stocks/FUNStockHolding.h
@@ -0,0 +1,13 @@
+//
+//  FUNStockHolding.h
+//  Stocks
+//
+//  Created by Jim Toepel on 10/20/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FUNStockHolding : NSObject
+
+@end
---
+++ Stocks/FUNStockHolding.m
@@ -0,0 +1,13 @@
+//
+//  FUNStockHolding.m
+//  Stocks
+//
+//  Created by Jim Toepel on 10/20/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import "FUNStockHolding.h"
+
+@implementation FUNStockHolding
+
+@end
