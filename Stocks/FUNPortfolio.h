5fb3aa8b3dfdc400308abfbd90e3b3520218a106
jimtoepel <jim.toepel@gmail.com>
2015-11-05 20:47:27 -0800

    started the challenge

---
+++ Stocks/FUNPortfolio.h
@@ -0,0 +1,15 @@
+//
+//  FUNPortfolio.h
+//  Stocks
+//
+//  Created by Jim Toepel on 11/5/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FUNPortfolio : NSObject
+
+-(float) totalValue;
+
+@end
---
+++ Stocks/FUNPortfolio.m
@@ -0,0 +1,18 @@
+//
+//  FUNPortfolio.m
+//  Stocks
+//
+//  Created by Jim Toepel on 11/5/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import "FUNPortfolio.h"
+
+@implementation FUNPortfolio
+
+-(float)totalValue
+{
+    return 0;
+}
+
+@end
