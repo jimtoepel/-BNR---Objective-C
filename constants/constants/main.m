31b63831c4fcf650c786524d467080013224de07
jimtoepel <jim.toepel@gmail.com>
2015-11-26 14:48:50 -0800

    Added constants!

--- constants/constants/main.m
+++ constants/constants/main.m
@@ -12,6 +12,11 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10,12));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
     }
     return 0;
 }
