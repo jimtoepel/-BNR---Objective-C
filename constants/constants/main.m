bd2e8a71bdf56970c22685e0d549efbb79f23bdc
jimtoepel <jim.toepel@gmail.com>
2015-11-26 13:57:27 -0800

    Kept the flame alive, love the family
    Constants!

---
+++ constants/constants/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  constants
+//
+//  Created by Jim Toepel on 11/26/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSLog(@"\u03c0 is %f", M_PI);
+    }
+    return 0;
+}
