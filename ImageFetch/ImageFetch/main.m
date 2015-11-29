4d47d86eaca504cc80e6cd9d327a1ddd3529afa8
jimtoepel <jim.toepel@gmail.com>
2015-11-29 12:37:27 -0800

    saved an image file!

---
+++ ImageFetch/ImageFetch/main.m
@@ -0,0 +1,41 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by Jim Toepel on 11/29/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSURL *url = [NSURL URLWithString:@"http://www.google.com/images/logos/ps_logo2.png"];
+        NSURLRequest * request = [NSURLRequest requestWithURL:url];
+        NSError *error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                             returningResponse:NULL
+                                                         error:&error];
+        
+        if (!data) {
+            NSLog(@"fetch failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"The file is %lu bytes", (unsigned long)[data length]);
+        
+        BOOL written = [data writeToFile:@"/tmp/google.png"
+                                 options:0
+                                   error:&error];
+        
+        if (!written) {
+            NSLog(@"write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+        
+        NSLog(@"Success!");
+        
+    }
+    return 0;
+}
