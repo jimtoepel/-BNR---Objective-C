ad7b3596aeefb1617c3c95563dde054a63f7779c
jimtoepel <jim.toepel@gmail.com>
2015-12-01 20:00:36 -0800

    Added an unused modifier

--- CallBacks/CallBacks/main.m
+++ CallBacks/CallBacks/main.m
@@ -14,7 +14,16 @@ int main(int argc, const char * argv[]) {
         
         FUNLogger *logger = [[FUNLogger alloc] init];
         
-        NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2.0
+        NSURL *url = [NSURL URLWithString:@"http://www.gutenberg.org/cache/epub/205/pg205.txt"];
+        
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        
+        __unused NSURLConnection *fetchConn = [[NSURLConnection alloc] initWithRequest:request
+                                                                              delegate:logger
+                                                                      startImmediately:YES];
+        
+        
+        __unused NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:2.0
                                                           target:logger
                                                         selector:@selector (updateLastTime:)
                                                         userInfo:nil
