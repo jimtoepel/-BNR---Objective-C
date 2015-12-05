47b2be89757f2efcb86201d35f3c7e3d3de117e6
jimtoepel <jim.toepel@gmail.com>
2015-12-05 13:20:59 -0800

    Vowels are OUT

--- VowelMovement/VowelMovement/main.m
+++ VowelMovement/VowelMovement/main.m
@@ -21,6 +21,30 @@ int main(int argc, const char * argv[]) {
         // Create a list of character to be removed from the string
         NSArray *vowels = @[@"a", @"e", @"i", @"o", @"u"];
         
+        // Declare the block variable
+        void (^devowelizer) (id, NSUInteger, BOOL *);
+        
+        // Compose a block and assign it to the variable
+        devowelizer = ^(id string, NSUInteger i, BOOL *stop) {
+            NSMutableString *newString = [NSMutableString stringWithString:string];
+            
+            // Iterate over the array of vowels, replacing occurences of each
+            // With an empty string(nothing)
+            
+            for (NSString *s in vowels) {
+                NSRange fullRange = NSMakeRange(0, [newString length]);
+                [newString replaceOccurrencesOfString:s
+                                           withString:@""
+                                              options:NSCaseInsensitiveSearch
+                                                range:fullRange];
+            }
+            [devowelizedStrings addObject:newString];
+        }; // End of Block assignment
+        
+        // Iterate over hte array with your block
+        [originalStrings enumerateObjectsUsingBlock:devowelizer];
+        NSLog(@"devowelized strings: %@", devowelizedStrings);
+        
     }
     return 0;
 }
