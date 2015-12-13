877e7725955a2814da196006dc49e5f540b70d5c
jimtoepel <jim.toepel@gmail.com>
2015-12-12 16:08:39 -0800

    Started the new list application
    Property lists!

---
+++ iTahList/iTahList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTahList
+//
+//  Created by Jim Toepel on 12/12/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTahList/iTahList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTahList
+//
+//  Created by Jim Toepel on 12/12/15.
+//  Copyright © 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
