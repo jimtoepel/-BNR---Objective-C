329ba654206e7c1b21db4373f2a5200fd3a8ca35
jimtoepel <jim.toepel@gmail.com>
2015-12-30 21:31:33 -0800

    some low level bit operations

--- bitwize/bitwize/main.c
+++ bitwize/bitwize/main.c
@@ -23,5 +23,18 @@ int main(int argc, const char * argv[]) {
     printf("Hex: %x & %x = %x\n", a, b, d);
     printf("Decimal: %d & %d = %d\n", a, b, d);
 
+    unsigned char f = ~b;
+    printf("Hex: The complement of %x is %x\n", b, f);
+    printf("Decimal: The complement of %d is %d\n", b, f);
+    
+    unsigned char g = a << 2;
+    printf("Hex: %x shifted left two places is %x\n", a, g);
+    printf("Decimal: %d shifted left two places is %d\n", a, g);
+    
+    unsigned char h = a >> 1;
+    printf("Hex: %x shifted right one place is %x\n", a, h);
+    printf("Decimal: %d shifted right one place is %d\n", a, h);
+    
+    
     return 0;
 }
